5b9b2ac55ecd59021a406fc61e6ca93d9e1d65dc
Ross Cooperman <ross@knowme.com>
2016-09-06 15:51:09 -0400

    Chapter 22

---
+++ NerdLauncher/app/src/main/java/com/knowme/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package com.knowme.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment f = fm.findFragmentById(R.id.fragment_container);
+        if (f == null) {
+            f = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, f).commit();
+        }
+    }
+}
